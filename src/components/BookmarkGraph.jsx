a9158491f94ffe726110f3fd23a7bc0cc6ecddf3
citron03 <cchan0617@gmail.com>
2022-03-20 17:29:57 +0900

    useEffect 에러 수정

--- src/components/BookmarkGraph.jsx
+++ src/components/BookmarkGraph.jsx
@@ -9,40 +9,41 @@ const BookmarkGraph = () => {
     // 랜덤 색상 만들기
     const getRandomColor = () => "#" + Math.floor(Math.random() * 16777215).toString(16);
     
-    let dataObj = {};
-    for(let i of state.cryptocurrency){
-        let str = i.data.payment + " to " + i.data.order;
-        if(dataObj[str]){
-            // 이미 있으면 추가
-            dataObj[str] = [i.data.price, ...dataObj[str]]
-        } else{
-            // 새로운 거래 내역
-            dataObj[str] = [i.data.price];
-        }
-    }
-
-    const labels = [1, 2, 3, 4, 5];
-    const data = {
-      labels: labels,
-      datasets: []
-    };
-    // datasets 설정
-    for(let i in dataObj){
-        data.datasets.push({
-            label: i, // 거래 종류
-            data: dataObj[i], // price
-            fill: false,
-            borderColor: getRandomColor(),
-            tension: 0.1
-        });
-    }
-    const config = {
-        type: 'line',
-        data: data,
-      };
-
     const canvas = useRef(null);
+
     useEffect(() => {
+        let dataObj = {};
+        for(let i of state.cryptocurrency){
+            let str = i.data.payment + " to " + i.data.order;
+            if(dataObj[str]){
+                // 이미 있으면 추가
+                dataObj[str] = [i.data.price, ...dataObj[str]]
+            } else{
+                // 새로운 거래 내역
+                dataObj[str] = [i.data.price];
+            }
+        }
+    
+        const labels = [1, 2, 3, 4, 5];
+        const data = {
+          labels: labels,
+          datasets: []
+        };
+        // datasets 설정
+        for(let i in dataObj){
+            data.datasets.push({
+                label: i, // 거래 종류
+                data: dataObj[i], // price
+                fill: false,
+                borderColor: getRandomColor(),
+                tension: 0.1
+            });
+        }
+        const config = {
+            type: 'line',
+            data: data,
+          };
+    
         // 처음 한번만 실행
         const ctx = canvas.current.getContext("2d");   
         new Chart(ctx, config);
